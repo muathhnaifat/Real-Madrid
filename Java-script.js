02dd5989b8edbc80771b8edbb943cbcd7fce7f82
Muath <muath_hnaifat@yahoo.com>
2019-10-03 01:19:34 +0300

    edit

--- Java-script.js
+++ Java-script.js
@@ -1,8 +1,20 @@
 
 
-var player = prompt("How many times has Real Madrid won the Champions League?");
-if(player == 13){
-  alert("correct answer");
-}else{
-  alert("sorry try again!");
-}
+
+
+// var player = prompt("How many times has Real Madrid won the Champions League?");
+// if(player == 13){
+//   alert("correct answer");
+// }else{
+//   alert("sorry try again!");
+// }
+  
+    var champion=prompt ("How many times has Real Madrid won the Champions League?");
+    
+    while (champion != 13) {
+    champion = prompt ("Sorry incorrect awnswer, please enter How many times has Real Madrid won the Champions League?");
+     }
+    if (champion == 13) {
+   alert ("THAT IS THE GREAT AWNSWER");
+} 
+    
